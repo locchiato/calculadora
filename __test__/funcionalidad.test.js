d3731d7267706cd6729d2ee2ee2fa5a43a9d0eb5
Leandro Occhiato <leandro.occhiato@gmail.com>
2021-07-11 20:58:05 -0300

    [MOD] Se agrega segundo test.

--- __test__/funcionalidad.test.js
+++ __test__/funcionalidad.test.js
@@ -1,15 +1,29 @@
-const { limpiar, resolver, init } = require('../funcionalidad');
-
+const { limpiar, resetear, resolver, init } = require('../funcionalidad');
 
 const id = 'resultado'
 const el = document.createElement('span')
 el.setAttribute('id', id)
-el.innerHTML = '10'
 document.body.appendChild(el)
 
-test('Probando el metodo limpiar', () => {
-    // const resultado = document.getElementById(id);
-    //expect(resultado.innerHTML).toBe('10');
-    limpiar();
-    // expect(resultado.innerHTML).toBe('');
+describe('Probando el metodo limpiar', function() {
+
+
+
+    test('Limpiar', () => {
+        const elToValidate = document.getElementById(id);
+        elToValidate.innerHTML = '10';
+        expect(elToValidate.innerHTML).toBe('10');
+        limpiar();
+        expect(elToValidate.innerHTML).toBe('');
+    });
+
+
+    test('Resetear', () => {
+        const elToValidate = document.getElementById(id);
+        elToValidate.innerHTML = '10';
+        expect(elToValidate.innerHTML).toBe('10');
+        resetear();
+        expect(elToValidate.innerHTML).toBe('');
+    });
+
 });
